7936c5348b30787aee8238afeb45e4020fc3d19a
cyvaer <cyvaer@gmail.com>
2022-03-24 19:51:07 -0600

    change server port from 3000 to 3001 due to possible conflicts with mac os proxies

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ init();
 
 async function init() {
   try {
-    app.listen(3001, () => {
-      console.log('Express App Listening on Port 3001');
+    app.listen(3000, () => {
+      console.log('Express App Listening on Port 3000');
     });
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
