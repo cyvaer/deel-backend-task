727fabfd45b7215cdffcf960f2c55e903be1d3c3
cyvaer <cyvaer@gmail.com>
2022-04-01 07:25:05 -0600

    modify auth middleware to reject the request when an invalid profile_id is entered

--- src/app.js
+++ src/app.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {sequelize} = require('./model')
-const {getProfile} = require('./middleware/getProfile')
 const app = express();
 app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
-/**
- * FIX ME!
- * @returns contract by id
- */
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-// Create app routes
 require('./modules/route')(app);
 
-app.get('/', getProfile, (req, res)=>{
-    console.log('getting profile', req.profile.dataValues);
-  res.send('hello root route').status(200);
-})
-
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
-})
 module.exports = app;
--- src/middleware/getProfile.js
+++ src/middleware/getProfile.js
@@ -1,9 +1,12 @@
-
+const util = require("../common/util");
 const getProfile = async (req, res, next) => {
     const {Profile} = req.app.get('models');
     const profile = await Profile.findOne({where: {id: req.query.profile_id || 0}});
-    if(!profile) return res.status(401).end();
-    req.profile = profile;
+    if(profile){
+        req.profile = {...profile.dataValues};
+    }else{
+        return util.badRequest(res, 'Unauthorized',401);
+    }
     next();
 }
-module.exports = {getProfile}
+module.exports = {getProfile};
