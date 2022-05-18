a2c0f33c1b6e2464c70ac8d749ca02b264632781
cyvaer <cyvaer@gmail.com>
2022-05-18 03:34:45 -0500

    Add note of intentional awareness of versioning keys for showing purposes

--- api-deel-task/src/common/config.js
+++ api-deel-task/src/common/config.js
@@ -1,5 +1,5 @@
 // const environment = require('../env.json');
-
+// I should not version this file but it is left intentional for showing proposes.
 const encryption = {
     KEY: 'HireMeDeel@',
     SUPERSECRETTIME: 900,
